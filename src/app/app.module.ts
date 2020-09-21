45de447ee470984c837f83585e6960f41963c83c
Dom-Marra <dominicmarra321@gmail.com>
2020-09-20 17:23:44 -0700

    Added module declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SimplebarAngularModule } from 'simplebar-angular';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { ProjectcardComponent } from './projectcard/projectcard.component';
+import { ProjectcarouselComponent } from './projectcarousel/projectcarousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    ProjectcardComponent,
+    ProjectcarouselComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    SimplebarAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]

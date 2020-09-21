bb9fc245e7eab70ae973798bd31132444b827b11
Dom-Marra <dominicmarra321@gmail.com>
2020-09-20 17:24:05 -0700

    Removed test projects

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { Project, Technologies } from '../project';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    
+  ]
 })
+
 export class HomeComponent implements OnInit {
 
   constructor() { }

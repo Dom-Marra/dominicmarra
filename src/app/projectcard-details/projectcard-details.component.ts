2df7f4e782ba0a2e8b374cc8352db10a1ad41b13
Dom-Marra <dominicmarra321@gmail.com>
2020-09-25 15:10:00 -0700

    Now accepts project data

--- src/app/projectcard-details/projectcard-details.component.ts
+++ src/app/projectcard-details/projectcard-details.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { MatDialogRef, MAT_DIALOG_DATA} from "@angular/material/dialog";
+import { Project } from '../project';
 
 @Component({
   selector: 'app-projectcard-details',
   templateUrl: './projectcard-details.component.html',
-  styleUrls: ['./projectcard-details.component.scss']
+  styleUrls: ['./projectcard-details.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ProjectcardDetailsComponent implements OnInit {
 
-  constructor() { }
+  public faGithub = faGithub; //github icon
+  public faLink = faLink;     //website link icon
+
+  public projectIcons: Array<any>;    //icons of the project
+  public project: Project;           //The project
+
+  constructor(private dialogRef: MatDialogRef<ProjectcardDetailsComponent>, @Inject(MAT_DIALOG_DATA) data) { 
+    this.project = data['project'];
+  }
 
   ngOnInit(): void {
+    //retreive the icons that belon to the project
+    this.projectIcons = this.project.getTechnologyIcons();
+  }
+
+  ngAfterViewInit() {
+  }
+
+  /**
+   * Closes the dialogue
+   */
+  public close() {
+    this.dialogRef.close()
   }
 
 }

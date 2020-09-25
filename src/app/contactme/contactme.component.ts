926c595c1f690f76a4752260ceada7341fd501db
Dom-Marra <dominicmarra321@gmail.com>
2020-09-25 15:00:27 -0700

    Added contact me page

---
+++ src/app/contactme/contactme.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-contactme',
+  templateUrl: './contactme.component.html',
+  styleUrls: ['./contactme.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ContactmeComponent implements OnInit {
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}

c1caeebf174b3c08c55f82bba1202f5e477a39b2
Dom-Marra <dominicmarra321@gmail.com>
2020-09-26 13:02:18 -0700

    Removed unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatButtonModule } from '@angular/material/button'
@@ -40,7 +40,6 @@ import { FooterComponent } from './footer/footer.component';
     FontAwesomeModule,
     MatDialogModule,
     SimplebarAngularModule,
-    FormsModule,
     ReactiveFormsModule,
     MatInputModule,
     MatFormFieldModule,

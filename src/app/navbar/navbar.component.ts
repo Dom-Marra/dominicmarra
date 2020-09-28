c41855c251ef228bf672d65ae7db51b7fdc42526
Dom-Marra <dominicmarra321@gmail.com>
2020-09-27 17:26:19 -0700

    Added active fragment service, updated commenting

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,23 +1,13 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { trigger, state, style, animate, transition } from '@angular/animations';
+import { ActiveFragService } from '../active-frag.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   animations: [
-    trigger("downUp", [ 
-      state("down", style({
-        transform: 'translateY(10px)',
-        color: "lightblue"
-      })),
-      state("up", style({
-        transform: 'translateY(0px)'
-      })),
-      transition('down <=> up', [
-        animate('0.1s')
-      ])
-    ]),
     trigger("shrinkEnlarge", [ 
       state("small", style({
         height: '{{navBarHeight}}px'
@@ -33,14 +23,16 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 })
 export class NavbarComponent implements OnInit {
 
+  @ViewChild('navContainer') navContainer: ElementRef;      //Nav container element
+
   @HostListener('window:scroll') 
   onScroll() {
 
     //Set navbar state depending on scroll position
-    if (window.scrollY >= 90 && this.navBarSize == "large") {
+    if (Math.floor(window.scrollY) > 90 && this.navBarSize == "large") {
       this.navBarSize = 'small';
       this.setNavBarHeight();
-    } else if (window.scrollY < 90 && this.navBarSize == "small") {
+    } else if (Math.floor(window.scrollY) < 75 && this.navBarSize == "small") {
       this.navBarSize = 'large';
       this.setNavBarHeight();
     }
@@ -63,23 +55,24 @@ export class NavbarComponent implements OnInit {
   }
 
   
-  public navItems = [                     //state of the navlinks
-    {state: "up"},
-    {state: "up"},
-    {state: "up"},
-  ];
-
-  
   public navBarSize: string = "large";    //state of the navbar container
   public navBarBaseHeight: number;        //navbar default height (changes depending if mobile or desktop)
   public navBarCurrentHeight: number;     //current height of the navbar
   public lastWindowSize: number;          //last known window size
 
-  constructor() { }
+  public activeFragment: BehaviorSubject<string>;     //Observable of current fragment in view
+  
+  constructor(private fragService: ActiveFragService) { 
+    this.activeFragment = this.fragService.activeFragment;
+  }
+
 
   ngOnInit(): void {
     this.lastWindowSize = window.innerWidth;
-    this.navBarBaseHeight = document.getElementById('navbar-container').offsetHeight;
+  }
+
+  ngAfterViewInit() {
+    this.navBarBaseHeight = this.navContainer.nativeElement.offsetHeight;
   }
 
   /**
@@ -98,26 +91,6 @@ export class NavbarComponent implements OnInit {
         break;
       }
     } 
-  }
-
-
-  /**
-   * Changes the state of the navlink when hovered over
-   * 
-   * @param navLinkIndex
-   *        The index of the nav link
-   */
-  hoverNavLink(navLinkIndex: number) {
-    this.navItems[navLinkIndex].state = "down";
-  }
-  /**
-   * Changes the state of the navlink when hovered over
-   * 
-   * @param navLinkIndex
-   *        The index of the nav link
-   */
-  leaveNavLink(navLinkIndex: number) {
-    this.navItems[navLinkIndex].state = "up";
   }
 
 }

666f33b8519bf26e0e5db6fe11c4643f75e26df9
Dom-Marra <dominicmarra321@gmail.com>
2020-09-27 17:20:47 -0700

    Updated comments

--- src/app/contactme/contactme.component.ts
+++ src/app/contactme/contactme.component.ts
@@ -10,7 +10,7 @@ import { Validators } from '@angular/forms';
 })
 export class ContactmeComponent implements OnInit {
 
-  contactForm = new FormGroup({
+  contactForm = new FormGroup({                                             //contact form values
     email: new FormControl('', [Validators.email, Validators.required]),
     subject: new FormControl('', Validators.required),
     description: new FormControl('', Validators.required)
@@ -22,10 +22,18 @@ export class ContactmeComponent implements OnInit {
     
   }
 
-  public submitForm(): void {
+  /**
+   * Submits the contact form
+   */
+  public submitForm(): void { 
     console.log(this.contactForm.value);
   }
 
+  /**
+   * Returns the error for email input
+   * 
+   * @returns String: message of the error
+   */
   public getEmailError(): String {
     if (this.contactForm.controls['email'].errors.required) {
       return "Please enter an email"
@@ -34,12 +42,22 @@ export class ContactmeComponent implements OnInit {
     }
   }
 
+  /**
+   * Returns the error for subject input
+   * 
+   * @returns String: message of the error
+   */
   public getSubjectError(): String { 
     if (this.contactForm.controls['subject'].errors.required) {
       return "Please enter a subject"
     }
   }
 
+  /**
+   * Returns the error for description input
+   * 
+   * @returns String: message of the error
+   */
   public getDescriptionError(): String { 
     if (this.contactForm.controls['description'].errors.required) {
       return "Please enter a description"

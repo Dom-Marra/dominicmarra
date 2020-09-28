3f7ecaa2e0129dd4686b63dd923a56a0beab64d4
Dom-Marra <dominicmarra321@gmail.com>
2020-09-27 17:33:22 -0700

    Added directive for setting the active fragment based on the dom element closest to the top of the view

---
+++ src/app/active-fragment.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ActiveFragmentDirective } from './active-fragment.directive';
+
+describe('ActiveFragmentDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ActiveFragmentDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/active-fragment.directive.ts
@@ -0,0 +1,42 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { ActiveFragService } from './active-frag.service';
+
+@Directive({
+  selector: '[appActiveFragment]'
+})
+export class ActiveFragmentDirective { 
+
+  @Input() offset: number = 0;                          //amount to offset from the top
+  
+  @HostListener('window:scroll')
+  onScroll() {
+      this.setActiveFragment();                         //update if the component is in view on scroll
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.setActiveFragment();                           //update if the component is in view on resize
+  }
+
+  constructor(private el: ElementRef, private fragService: ActiveFragService) { 
+  }
+
+  ngAfterViewInit(): void {
+    this.setActiveFragment();
+  }
+
+  /**
+   * Updates the active fragment form the fragment service if the component is in view
+   */
+  public setActiveFragment() {
+
+      let compTop = this.el.nativeElement.offsetTop;                  //components top value
+      let compBottom = compTop + this.el.nativeElement.offsetHeight;  //components bottom value
+
+      //calculate if the top portion of the window is within component
+      if (Math.ceil(window.scrollY) + this.offset > compTop && compBottom > Math.ceil(window.scrollY) + this.offset) {
+        this.fragService.setActiveFrag(this.el.nativeElement.id);     //update frag service active fragment
+      } 
+  }
+
+}

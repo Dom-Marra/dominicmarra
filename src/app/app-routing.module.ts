eb3304cf385a2a97dae6bb8b5ebd27699f198124
Dom-Marra <dominicmarra321@gmail.com>
2020-09-27 17:19:20 -0700

    updated router options

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 import { HomeComponent } from '../app/home/home.component';
 
-const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  { path: '',   redirectTo: '/home', pathMatch: 'full'}
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'disabled',
+  anchorScrolling: 'disabled',
+  onSameUrlNavigation: 'reload'
+};
 
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

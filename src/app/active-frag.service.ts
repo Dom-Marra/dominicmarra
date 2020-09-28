3d311ae5ee03a03fdc7e4e466dcfe1cf336f4c2b
Dom-Marra <dominicmarra321@gmail.com>
2020-09-28 16:35:21 -0700

    Now uses activefragment class for tracking

--- src/app/active-frag.service.ts
+++ src/app/active-frag.service.ts
@@ -1,30 +1,30 @@
-import { ThrowStmt } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { Router, RoutesRecognized } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 import { filter } from 'rxjs/operators';
+import { Activefragment } from './activefragment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActiveFragService {
 
-  public activeFragment = new BehaviorSubject<string>('');
+  public activeFragment = new BehaviorSubject<Activefragment>(new Activefragment('', false));
   constructor(private router: Router) { 
 
     //Set the active fragment on route
     this.router.events.pipe(filter(event => event instanceof RoutesRecognized)).subscribe((route: RoutesRecognized) => {
-      this.setActiveFrag(route.state.root.fragment);
+      this.setActiveFrag(new Activefragment(route.state.root.fragment, true));
     });
   }
 
   /**
-   * Sets the active frag ment
+   * Sets the active fragment
    * 
    * @param frag 
    *        the fragment
    */
-  public setActiveFrag(frag: string): void {
+  public setActiveFrag(frag: Activefragment): void {
     if (this.activeFragment.getValue() != frag) {
       this.activeFragment.next(frag);
     }

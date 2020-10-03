db56ba3828487f4e741df05efc9a4b7314708a36
Dom-Marra <dominicmarra321@gmail.com>
2020-10-02 20:21:27 -0700

    Added firebase variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAPTQsShZcQPgJTuTjLvYiewfRZGsqcilM",
+    authDomain: "dominicmarra-95b72.firebaseapp.com",
+    databaseURL: "https://dominicmarra-95b72.firebaseio.com",
+    projectId: "dominicmarra-95b72",
+    storageBucket: "dominicmarra-95b72.appspot.com",
+    messagingSenderId: "135299901252",
+    appId: "1:135299901252:web:bc001669f3556a46425911",
+    measurementId: "G-88R7ZXTP9T"
+  }
 };
 
 /*

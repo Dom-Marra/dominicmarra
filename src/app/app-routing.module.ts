1ffaf0193f5c8efb4fea5ecc76a32a298e252b3c
Dom-Marra <dominicmarra321@gmail.com>
2020-10-02 20:22:48 -0700

    Added routes for console components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 import { HomeComponent } from '../app/home/home.component';
+import { LoginComponent } from '../app/login/login.component';
+import { ConsoleComponent } from '../app/console/console.component';
+import { AddprojectComponent } from '../app/console/addproject/addproject.component';
+import { DeleteprojectComponent } from '../app/console/deleteproject/deleteproject.component';
+import { UpdateprojectComponent } from '../app/console/updateproject/updateproject.component';
+
+import { AngularFireAuthGuard, canActivate, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToConsole = () => redirectLoggedInTo(['console']);
 
 const routerOptions: ExtraOptions = {
   scrollPositionRestoration: 'disabled',
@@ -11,7 +21,16 @@ const routerOptions: ExtraOptions = {
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, ...canActivate(redirectLoggedInToConsole)},
+  { path: 'console', component: ConsoleComponent, ...canActivate(redirectUnauthorizedToLogin),
+    children: [
+      { path: '', redirectTo: 'addproject', pathMatch: 'full'},
+      { path: 'addproject', component: AddprojectComponent },
+      { path: 'updateproject', component: UpdateprojectComponent },
+      { path: 'deleteproject', component: DeleteprojectComponent }
+
+    ] },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({

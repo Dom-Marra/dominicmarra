60144e3abad1eaaec43edc9c1b125d2d4393463a
Dom-Marra <dominicmarra321@gmail.com>
2020-10-05 01:34:49 -0700

    Finished functionality for adding projects

--- src/app/console/projectform/projectform.component.ts
+++ src/app/console/projectform/projectform.component.ts
@@ -1,7 +1,8 @@
-import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
+import { FormGroup, FormControl, ValidationErrors } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { Project, Technologies } from '../../project';
+import { FirebaseService } from '../../firebase.service';
 
 @Component({
   selector: 'app-projectform',
@@ -9,15 +10,39 @@ import { Project, Technologies } from '../../project';
   styleUrls: ['./projectform.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
+
 export class ProjectformComponent implements OnInit {
 
-  @ViewChild('thumbnail') thumbnailImageBox: ElementRef<HTMLElement>;
+  @Output() projectFormEmitted = new EventEmitter<Project>();   //Event emmiter to push the project on form submit
+
+  @Input() project: Project;                                    //project to use during modification
+
+  @ViewChild('thumbnail') thumbnailImageBox: ElementRef<HTMLElement>;   //thumbnail preview div
 
   public images: Array<string | ArrayBuffer> = [];
   public techs = Technologies;
+
+  public toolbarOptions = {                               //Quill editor toolbar options
+
+    toolbar: {
+      container: [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+      
+        [{ 'header': 1 }],                                // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'direction': 'rtl' }],                         // text direction
+      
+        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        [{ 'header': [1, 2, 3] }],
+      
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'align': [] }],
+      ]
+    }
+  };
   
 
-  projectForm = new FormGroup({
+  public projectForm = new FormGroup({
     thumbnail: new FormControl('', Validators.required),
     images: new FormControl('', Validators.required),
     name: new FormControl('', Validators.required),
@@ -25,11 +50,13 @@ export class ProjectformComponent implements OnInit {
     description: new FormControl('', Validators.required),
     technologies: new FormControl('', Validators.required),
     color: new FormControl('', Validators.required),
-    projectlink: new FormControl(''),
-    githublink: new FormControl(''),
-  })
+    projectlink: new FormControl(null),
+    githublink: new FormControl(null),
+  });
 
-  constructor() { }
+  private imageInvalidErrorNames: Array<String> = [];                 //names of files that do not match an image type
+
+  constructor(private firebase: FirebaseService) { }
 
   ngOnInit(): void { }
 
@@ -37,43 +64,132 @@ export class ProjectformComponent implements OnInit {
    * Submits the project form
    */
   public submitForm() {
-    console.log(this.projectForm.value);
+    let project = new Project(this.projectForm.controls.name.value, 
+                              this.projectForm.controls.caption.value, 
+                              this.projectForm.controls.thumbnail.value, 
+                              this.projectForm.controls.color.value.hex, 
+                              this.projectForm.controls.description.value, 
+                              this.projectForm.controls.images.value, 
+                              this.projectForm.controls.technologies.value, 
+                              this.projectForm.controls.projectlink.value, 
+                              this.projectForm.controls.githublink.value);
+    
+    this.projectFormEmitted.emit(project);
+  }
+
+
+  /**
+   * Handles the file input change event fired from the thumbnail file input element
+   * 
+   * @param event 
+   *        The on change event fired from the file input
+   */
+  public thumbnailInputChange(event) {
+    this.projectForm.get('thumbnail').setErrors(null);                      //reset errors on thumbnail
+   
+    const file: File = this.projectForm.controls.thumbnail.value;           //The file to read
+
+    this.validateImage(file).then(() => {                       
+      this.displayThumbnail(file);
+    }).catch(err => {                                                         //Validation failed                                           
+      event.target.value = null;                                              //clear file input
+      this.thumbnailImageBox.nativeElement.style.backgroundImage = '';        //clear thumbnail element background image
+      this.projectForm.controls.thumbnail.setValue(null);                     //clear thumbnail value
+      this.projectForm.get('thumbnail').setErrors(err);                       //set thumbnail error
+    })
+  }
+
+  /**
+   * Validates an image from the file input. An image is valid if the file type matches an image file.
+   * 
+   * @param image 
+   *        File: image to validate
+   * 
+   * @returns
+   *         Promise<ValidationErrors>: The error if one exists
+   */
+  public validateImage(image: File): Promise<ValidationErrors> {
+
+    let validationPromise = new Promise<ValidationErrors>((resolve, reject) => {
+      if (!image.type.match('image.*')) {                                   //rejects false if the file is not an image type
+        this.imageInvalidErrorNames.push(image.name);
+        reject({'InvalidFile': true});
+        return;
+      } else {
+        resolve(null);
+      }
+    });
+    
+    return validationPromise;
   }
 
   /**
    * Displays the inputed image for the project thumbnail
    * 
-   * @param event 
-   *        The on change event fired from the file input
+   * @param image 
+   *        File: image to display
    */
-  public displayThumbnail(event): void {
-
-    this.projectForm.get('thumbnail').setErrors(null);                      //Set the thumbnail input element error to none
+  public displayThumbnail(image: File): void {
     const filereader = new FileReader();                                    //File reader for the thumbnail input
 
-    const fileAsBlob: File = this.projectForm.controls.thumbnail.value;     //The file to read
-    const file = fileAsBlob as Blob;                                    
-
-    if (file == null) {
-      this.projectForm.controls.thumbnail.setValue(null);
-      this.thumbnailImageBox.nativeElement.style.backgroundImage = '';
-      return;
-    }
-
-    if (!file.type.match('image.*')) {                                      //If the file is not of an image type, 
-      event.target.value = null;                                            //set error and clear the input and return
-      this.thumbnailImageBox.nativeElement.style.backgroundImage = '';
-      this.projectForm.controls.thumbnail.setValue(null);
-      this.projectForm.get('thumbnail').setErrors({'InvalidFile': true});
-      return;
-    }
-
-    filereader.readAsDataURL(file);                        //read the image
-
+    filereader.readAsDataURL(image);                                        //read the image
+  
     filereader.onload = () => {                                             //Set the thumbnail preview div background
       this.thumbnailImageBox.nativeElement.style.backgroundImage = 'url(' + filereader.result + ')';
     }
   }
+
+  /**
+   * Handles the file input change event fired from the images file input element
+   * 
+   * @param event 
+   *        The on change event fired from the file input
+   */
+  public imagesInputChange(event) { 
+    this.images = [];                                                        //clear images array
+    this.projectForm.get('images').setErrors(null);                          //reset errors on images
+
+    const files = this.projectForm.controls.images.value;                     //The files to read
+    
+    this.imageInvalidErrorNames = [];                                         
+
+    this.validateImages(files).then(() => {                                   //validate images
+      this.displayImages(files);
+    }).catch((err: ValidationErrors) => {                                     //Validation failed
+      event.target.value = null;                                              //cleat the file input
+      this.projectForm.controls.images.setValue(null);                        //clear images form value
+      this.images = [];                                                       //clear images array
+      this.projectForm.get('images').setErrors(err);                          //set images form error
+    })
+  }
+
+  /**
+   * Validates an array of images from the file input. An image is valid if the file type matches an image file.
+   * 
+   * @param images
+   *        Array<File>: images to validate
+   * 
+   * @returns
+   *         Promise<ValidationErrors>: The error if one exists
+   */
+  public validateImages(images: Array<File>): Promise<ValidationErrors> {
+    let validationPromises = [];                                    //Array to hold promises of validation
+     
+    for (let image of images) {                                     //validate each image
+      validationPromises.push(this.validateImage(image));
+    }
+
+    let validationPromise = new Promise((resolve, reject) => {     //validation promise of all image validations
+      Promise.all(validationPromises).then(() => {
+        resolve(null);                                            //no error, resolve null
+      }).catch(err => {
+        reject(err);                                              //Error occured, reject with it 
+      })
+    })
+
+    return validationPromise;
+  }
+
 
   /**
    * Displays images of inputted files
@@ -81,30 +197,17 @@ export class ProjectformComponent implements OnInit {
    * @param event 
    *        The on change event fired from the file input
    */
-  public displayImages(event): void {
+  public displayImages(images: Array<File>): void {
 
-    this.images = [];
-    this.projectForm.get('images').setErrors(null);
-
-    const files = this.projectForm.controls.images.value;                         //The files to read
-
-    for (let file of files) {
-      if (!file.type.match('image.*')) {                                          //If the file is not of an image type, 
-        event.target.value = null;                                                //set error and clear the input and return
-        this.projectForm.controls.images.setValue(null);
-        this.images = [];
-        this.projectForm.get('images').setErrors({'containsInvalidFiles': true});
-        return;
-      }
-    }
-
-    for (let file of files) {                                                     //set images to display
+    //Iterate through the image files, read and 
+    //push them to the images array
+    images.forEach(image => {
       let filereader = new FileReader();
-      filereader.readAsDataURL(file);
+      filereader.readAsDataURL(image);
       filereader.onload = () => {
         this.images.push(filereader.result);
       };
-    }
+    })
   }
 
   /**
@@ -128,8 +231,8 @@ export class ProjectformComponent implements OnInit {
     if (this.projectForm.controls.thumbnail.errors.InvalidFile) {
       return 'The inputed file was not an image type!'
     } else if (this.projectForm.controls.thumbnail.errors.required) {
-      return 'Please input an image'
-    }
+      return 'Please input an image';
+    } 
   }
 
   /**
@@ -139,12 +242,12 @@ export class ProjectformComponent implements OnInit {
    *        string: error
    */
   public getImagesErrors(): string {
-    if (this.projectForm.controls.images.errors.containsInvalidFiles) {
-      return 'An inputed file was not an image type!'
+    
+    if (this.projectForm.controls.images.errors.InvalidFile) {
+      return 'The following files were not an image type: ' + this.imageInvalidErrorNames;
     } else if (this.projectForm.controls.images.errors.required) {
-      return 'Please input at least one image'
-    }
+      return 'Please input at least one image';
+    } 
   }
-
 
 }

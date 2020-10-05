b6ea1c12c5b05a28a1f0bd062ec5f32e0141d1ce
Dom-Marra <dominicmarra321@gmail.com>
2020-10-05 01:33:14 -0700

    Updated for firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAPTQsShZcQPgJTuTjLvYiewfRZGsqcilM",
+    authDomain: "dominicmarra-95b72.firebaseapp.com",
+    databaseURL: "https://dominicmarra-95b72.firebaseio.com",
+    projectId: "dominicmarra-95b72",
+    storageBucket: "dominicmarra-95b72.appspot.com",
+    messagingSenderId: "135299901252",
+    appId: "1:135299901252:web:bc001669f3556a46425911",
+    measurementId: "G-88R7ZXTP9T"
+  }
 };

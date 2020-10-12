1afbb22b9fa43e6f037f8642dbf9148540cfcca3
Dom-Marra <dominicmarra321@gmail.com>
2020-10-12 11:24:06 -0700

    Rmoeved and cleaned the code and handles different scenarios from the active frag service to resolve issues when navigating to pages that dont have a fragment

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,110 +1,31 @@
-import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActiveFragService } from '../active-frag.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss'],
-  animations: [
-    trigger("shrinkEnlarge", [ 
-      state("small", style({
-        height: '{{navBarHeight}}px'
-      }), {params: {navBarHeight: 75}}),
-      state("large", style({
-        height: '{{navBarHeight}}px'
-      }), {params: {navBarHeight: 90}}),
-      transition('small <=> large', [
-        animate('0.1s')
-      ])
-    ])
-  ]
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
   @ViewChild('navContainer') navContainer: ElementRef;      //Nav container element
 
-  @HostListener('window:scroll') onScroll(): void {
-    this.setNavBarState();
-  }
-
-  @HostListener('window:resize') onResize(e: Event): void {
-    this.setNavBarBaseHeight();
-  }
-
-  
-  public navBarSize: string = "large";    //state of the navbar container
-  public navBarBaseHeight: number;        //navbar default height (changes depending if mobile or desktop)
-  public navBarCurrentHeight: number;     //current height of the navbar
-  public lastWindowSize: number;          //last known window size
-
-  public activeFragment: string;                  //Observable of current fragment in view
+  public activeFragment: string = null;                     //Observable of current fragment in view
   
   constructor(private fragService: ActiveFragService) { 
     this.fragService.activeFragment.subscribe(frag => {
-      if (!frag.getWasRouted() && frag.getFragment() != this.activeFragment) {
+      if (frag.getWasRouted() && this.activeFragment == null) {
         this.activeFragment = frag.getFragment();
+      } else if (!frag.getWasRouted() && frag.getFragment() != this.activeFragment) {
+        this.activeFragment = frag.getFragment();
+      } else if (frag.getWasRouted() && frag.getFragment() == null) {
+        this.activeFragment = null;
       }
     });
-
-    this.setNavBarBaseHeight();
-    this.setNavBarState();
   }
 
 
   ngOnInit(): void {
-    this.lastWindowSize = window.innerWidth;
-  }
-
-  ngAfterViewInit(): void {
-    this.navBarBaseHeight = this.navContainer.nativeElement.offsetHeight;
-  }
-
-  /**
-   * Sets the navbar containers height, which is used as a parameter in its
-   * animations
-   */
-  private setNavBarHeight(): void {
-
-    switch (this.navBarSize) {
-      case "large" : {
-        this.navBarCurrentHeight = this.navBarBaseHeight;
-        break;
-      }
-      case "small": {
-        this.navBarCurrentHeight = this.navBarBaseHeight * 0.9;
-        break;
-      }
-    } 
-  }
-
-  /**
-   * Set navbar state depending on scroll position
-   */
-  private setNavBarState(): void {
-
-    if (Math.floor(window.scrollY) > 90 && this.navBarSize == "large") {
-      this.navBarSize = 'small';
-      this.setNavBarHeight();
-    } else if (Math.floor(window.scrollY) < 75 && this.navBarSize == "small") {
-      this.navBarSize = 'large';
-      this.setNavBarHeight();
-    }
-  }
-
-  /**
-   * Set the navbar height based on window size
-   */
-  private setNavBarBaseHeight(): void {
-    if (window.innerWidth > 790 && (this.lastWindowSize <= 790 || this.lastWindowSize == null)) {
-      this.navBarBaseHeight = 90;
-      this.lastWindowSize = window.innerWidth;
-      this.setNavBarHeight();
-    } else if (window.innerWidth <= 790 && (this.lastWindowSize > 790 || this.lastWindowSize == null)) {
-      this.navBarBaseHeight = 110;
-      this.lastWindowSize = window.innerWidth;
-      this.setNavBarHeight();
-    }
   }
 
   /**

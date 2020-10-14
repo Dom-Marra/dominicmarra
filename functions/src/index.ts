a1d27467a290d0c0b099c25f3143aa58e09b37e2
Dom-Marra <dominicmarra321@gmail.com>
2020-10-13 18:40:55 -0700

    added email function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,38 @@
-//import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+import * as nodemailer from 'nodemailer';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+export const sendEmail = functions.https.onRequest((request, response) => {
+    const cors = require('cors')({origin: true});
+
+    cors(request, response, () => {
+        const gmailEmail = 'dominicmarra321@gmail.com';
+        const gmailPassword = functions.config().emailservice.secretpassword;
+        let content = JSON.parse(request.body);
+
+        const mailTransport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: gmailEmail,
+                pass: gmailPassword,
+            },
+        });
+
+        if (!content.hasOwnProperty('email') || !content.hasOwnProperty('subject') || !content.hasOwnProperty('description')) {
+            response.status(500).send(false);
+        }
+
+        const mailOptions: nodemailer.SendMailOptions = {
+            to: gmailEmail,
+            subject: content['subject'],
+            text: 'From: ' + content['email'] + '\n\n' + content['description']
+        };
+
+        mailTransport.sendMail(mailOptions).then(() => {
+            response.status(200).send(true);
+        }).catch(() => {
+            response.status(500).send(false);
+        });
+    });
+    
+});
